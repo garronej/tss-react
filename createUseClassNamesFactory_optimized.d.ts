1f055a7ecaed0a03de39e98fb53ab69cc3abe07f
ts_ci <ts_ci@github.com>
2021-02-24 13:52:01 +0000

    Enabling shorter import paths [automatic]

---
+++ createUseClassNamesFactory_optimized.d.ts
@@ -0,0 +1,37 @@
+import "minimal-polyfills/Object.fromEntries";
+import type { CSSObject } from "@emotion/css";
+/**
+ * !EXPERIMENTAL!
+ *
+ * Functionally equivalent from the exported but with optimization
+ * mechanism implemented.
+ *
+ * Implementation of JSS createStyles() based on @emotion/react
+ *
+ * const { useCssRecord } = createUseClassNames<Props & { color: "red" | "blue" }>()({
+ *    (theme, { color })=> ({
+ *        "root": { color }
+ *    })
+ * });
+ *
+ *
+ * function MyComponent(props: Props){
+ *
+ *     const [ color, setColor ]= useState<"red" | "blue">("red");
+ *
+ *     const { classNames }=useClassName({...props, color });
+ *
+ *     return <span className={classNames.root}>hello world</span>;
+ *
+ * }
+ *
+ */
+export declare function createUseClassNamesFactory<Theme extends Object = {}>(params: {
+    useTheme(): Theme;
+}): {
+    createUseClassNames: <Params extends Record<string, unknown>>() => <Key extends string>(getCssObjects: (theme: Theme, params: Params) => Record<Key, CSSObject>) => {
+        useClassNames: (params: Params) => {
+            classNames: Record<Key, string>;
+        };
+    };
+};
