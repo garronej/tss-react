87163642e95f8571a3e85062e3bdc2460dda06dd
Joseph Garrone <joseph.garrone.gj@gmail.com>
2021-03-05 23:48:59 +0100

    Fix the type definition of cx

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
 
-export * from "@emotion/css";
 
-import { css } from "@emotion/css";
+export type { ArrayCSSInterpolation, CSSObject, CSSInterpolation, CSSStyleSheet, ComponentSelector, EmotionCache } from "@emotion/css";
+export  { cache, flush, getRegisteredStyles, hydrate, injectGlobal, keyframes, merge, sheet } from "@emotion/css";
+import { cx as cx_ } from "@emotion/css";
+
+export interface ArrayClassNamesArg extends Array<ClassNamesArg> {}
+
+export type ClassNamesArg =
+  | undefined
+  | null
+  | string
+  | boolean
+  | { [className: string]: boolean | null | undefined }
+  | ArrayClassNamesArg;
+
+//SEE: https://github.com/emotion-js/emotion/pull/2276
+export const cx: (...classNames: Array<ClassNamesArg>)=>  string = cx_ as any;
+
+import { css } from "@emotion/css"; export { css };
 import { createUseClassNamesFactory as createUseClassNamesFactoryNeedsCssFn } from "./createUseClassNamesFactory";
 
 /** https://github.com/garronej/tss-react */
