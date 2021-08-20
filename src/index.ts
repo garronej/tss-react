4641d807d8a25180545ea69314cafbc7840e5785
garronej <joseph.garrone.gj@gmail.com>
2021-08-20 16:28:38 +0200

    Expose useCssAndCx for project with no theme

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import { createMakeStyles as createMakeStylesBase } from "./createMakeStyles";
 export type { CSSInterpolation, CSSObject, Css, Cx, CxArg } from "./types";
-import { createUseCssAndCx } from "./cssAndCx";
+import { createUseCssAndCx as createUseCssAndCx_needsDependencyInjection } from "./cssAndCx";
 import { serializeStyles } from "@emotion/serialize";
 import { insertStyles, getRegisteredStyles } from "@emotion/utils";
 import type { EmotionCache } from "@emotion/cache";
 import { useEmotionCache } from "./@emotion/react";
 import { getCache } from "./cache";
+
+function createUseCssAndCx(params: { useEmotionCache(): EmotionCache }) {
+    const { useEmotionCache } = params;
+
+    return createUseCssAndCx_needsDependencyInjection({
+        getRegisteredStyles,
+        insertStyles,
+        serializeStyles,
+        useEmotionCache,
+    });
+}
 
 /** https://github.com/garronej/tss-react */
 export function createMakeStyles<Theme>(params: {
@@ -15,9 +26,6 @@ export function createMakeStyles<Theme>(params: {
     const { useTheme, cache } = params;
 
     const { useCssAndCx } = createUseCssAndCx({
-        getRegisteredStyles,
-        insertStyles,
-        serializeStyles,
         "useEmotionCache":
             cache !== undefined
                 ? () => cache
@@ -42,3 +50,8 @@ export function createMakeStyles<Theme>(params: {
 export { keyframes } from "@emotion/react";
 
 export { GlobalStyles } from "./GlobalStyles";
+
+/** Will pickup the cache from the context or use the default cache */
+export const { useCssAndCx } = createUseCssAndCx({
+    "useEmotionCache": () => useEmotionCache() ?? getCache(),
+});
