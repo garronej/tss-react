dcbb1c31c8df4bd1fcfa8736c78805dcb9b06175
garronej <joseph.garrone.gj@gmail.com>
2021-11-23 09:40:33 +0100

    Avoid using JSON.stringify in getDependencyArrayRef #39

--- src/tools/getDependencyArrayRef.ts
+++ src/tools/getDependencyArrayRef.ts
@@ -28,11 +28,13 @@ export function getDependencyArrayRef(obj: any): any {
     for (const key in obj) {
         const value = obj[key];
 
+        const typeofValue = typeof value;
+
         if (
             !(
-                typeof value === "string" ||
-                (typeof value === "number" && !isNaN(value)) ||
-                typeof value === "boolean" ||
+                typeofValue === "string" ||
+                (typeofValue === "number" && !isNaN(value)) ||
+                typeofValue === "boolean" ||
                 value === undefined ||
                 value === null
             )
@@ -40,7 +42,7 @@ export function getDependencyArrayRef(obj: any): any {
             return obj;
         }
 
-        arr.push(`${key}:${JSON.stringify(value)}`);
+        arr.push(`${key}:${typeofValue}_${value}`);
     }
 
     return "xSqLiJdLMd9s" + arr.join("|");
