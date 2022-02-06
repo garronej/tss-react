c6f9f469eb22bc255b9c915e22f37e1c6a57cde6
garronej <joseph.garrone.gj@gmail.com>
2022-02-06 09:37:15 +0100

    Fix compat index

--- src/mergeClasses.ts
+++ src/mergeClasses.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import type { Cx } from ".";
+import type { Cx } from "./types";
 import { objectKeys } from "./tools/objectKeys";
 import { useCssAndCx } from "./cssAndCx";
 import { useMemo } from "react";
