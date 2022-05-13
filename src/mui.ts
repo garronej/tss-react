8d85147dba89cce7947e282534eae4bd71a42b65
garronej <joseph.garrone.gj@gmail.com>
2022-05-13 12:54:08 +0200

    We do not need to export sxToCSSObject

--- src/mui.ts
+++ src/mui.ts
@@ -2,23 +2,15 @@ import { useTheme } from "@mui/material/styles";
 import { createMakeAndWithStyles } from "./index";
 import { unstable_styleFunctionSx as styleFunctionSx } from "@mui/system";
 import type { CSSObject } from "./types";
-import { SxProps, Theme } from "@mui/material/styles";
 
 /** @see <https://docs.tss-react.dev/setup> */
 export const { makeStyles, withStyles, useStyles } = createMakeAndWithStyles({
     useTheme,
 });
 
-export const sxToCSSObject = (params: {
-    customObject: SxProps<Theme>;
-    theme: Theme;
-}) => {
-    const { customObject: sx, theme } = params;
-    return styleFunctionSx({ sx, theme }) as CSSObject;
-};
-
 export const { makeStyles: makeSxStyles, withStyles: withSxStyles } =
     createMakeAndWithStyles({
         useTheme,
-        "customObjectToCSSObject": sxToCSSObject,
+        "customObjectToCSSObject": ({ customObject: sx, theme }) =>
+            styleFunctionSx({ sx, theme }) as CSSObject,
     });
