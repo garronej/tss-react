691adb94078d7bef8e77ca1a9d53f81b3020c43b
garronej <joseph.garrone.gj@gmail.com>
2022-07-10 16:23:43 +0200

    Internalize shenanigans relative to setting up SSR with next

--- src/nextJs.tsx
+++ src/nextJs.tsx
@@ -1,54 +1,75 @@
 import * as React from "react";
+import type { ReactNode } from "react";
 import createEmotionServer from "@emotion/server/create-instance";
 import type NextDocument from "next/document";
 import type { DocumentContext } from "next/document";
 import type { EmotionCache } from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
+import type { Options as CreateCacheOption } from "@emotion/cache";
+import createCache from "@emotion/cache";
 
 /** @se <https://docs.tss-react.dev/ssr/next.js> */
-export function withEmotionCache(params: {
-    Document: typeof NextDocument;
-    getCaches: () => EmotionCache[];
-}): typeof NextDocument {
-    const { Document, getCaches } = params;
-    return class DocumentWithEmotionCache extends Document {
-        static async getInitialProps(ctx: DocumentContext) {
-            const emotionServers = getCaches()
-                .sort((a, b) =>
-                    getPrepend(a) === getPrepend(b)
-                        ? 0
-                        : getPrepend(a)
-                        ? -1
-                        : 1,
-                )
-                .map(createEmotionServer);
+export function createEmotionSsrAdvancedApproach(options: CreateCacheOption) {
+    let muiCache: EmotionCache | undefined = undefined;
 
-            const initialProps = await Document.getInitialProps(ctx);
+    const createMuiCache = () => (muiCache = createCache(options));
 
-            return {
-                ...initialProps,
-                "styles": [
-                    ...React.Children.toArray(initialProps.styles),
-                    ...emotionServers
-                        .map(({ extractCriticalToChunks }) =>
-                            extractCriticalToChunks(initialProps.html)
-                                .styles.filter(({ css }) => css !== "")
-                                .map(style => (
-                                    <style
-                                        data-emotion={`${
-                                            style.key
-                                        } ${style.ids.join(" ")}`}
-                                        key={style.key}
-                                        dangerouslySetInnerHTML={{
-                                            "__html": style.css,
-                                        }}
-                                    />
-                                )),
-                        )
-                        .reduce((prev, curr) => [...prev, ...curr], []),
-                ],
-            };
-        }
-    };
+    function EmotionCacheProvider(props: { children: ReactNode }) {
+        const { children } = props;
+        return (
+            <CacheProvider value={muiCache ?? createMuiCache()}>
+                {children}
+            </CacheProvider>
+        );
+    }
+
+    function withEmotionCache(params: {
+        Document: typeof NextDocument;
+        getExtraCaches?: () => EmotionCache[];
+    }) {
+        const { Document, getExtraCaches = () => [] } = params;
+        return class DocumentWithEmotionCache extends Document {
+            static async getInitialProps(ctx: DocumentContext) {
+                const emotionServers = [createMuiCache(), ...getExtraCaches()]
+                    .sort((a, b) =>
+                        getPrepend(a) === getPrepend(b)
+                            ? 0
+                            : getPrepend(a)
+                            ? -1
+                            : 1,
+                    )
+                    .map(createEmotionServer);
+
+                const initialProps = await Document.getInitialProps(ctx);
+
+                return {
+                    ...initialProps,
+                    "styles": [
+                        ...React.Children.toArray(initialProps.styles),
+                        ...emotionServers
+                            .map(({ extractCriticalToChunks }) =>
+                                extractCriticalToChunks(initialProps.html)
+                                    .styles.filter(({ css }) => css !== "")
+                                    .map(style => (
+                                        <style
+                                            data-emotion={`${
+                                                style.key
+                                            } ${style.ids.join(" ")}`}
+                                            key={style.key}
+                                            dangerouslySetInnerHTML={{
+                                                "__html": style.css,
+                                            }}
+                                        />
+                                    )),
+                            )
+                            .reduce((prev, curr) => [...prev, ...curr], []),
+                    ],
+                };
+            }
+        };
+    }
+
+    return { EmotionCacheProvider, withEmotionCache };
 }
 
 function getPrepend(cache: EmotionCache): boolean {
