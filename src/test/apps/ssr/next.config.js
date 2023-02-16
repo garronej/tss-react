01feacbe9ff3f8ebec3ce2b2f098d29649f850b6
Michiel <michiel.hennekam@vakanties.nl>
2023-02-16 08:39:06 +0100

    fix: indenting on nextjs file for test

--- src/test/apps/ssr/next.config.js
+++ src/test/apps/ssr/next.config.js
@@ -1,19 +1,17 @@
-
 const path = require("path");
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
+    enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = 
-	withBundleAnalyzer({
-  reactStrictMode: true,
-  webpack: function (config, { isServer, webpack }) {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-    };
+module.exports = withBundleAnalyzer({
+    reactStrictMode: true,
+    webpack: function (config, { isServer, webpack }) {
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            fs: false,
+        };
 
-    return config;
-  },
+        return config;
+    },
 });
