34daff1cf54c83452c350df4dc36abae0b0b7e96
Michiel <michiel.hennekam@vakanties.nl>
2023-02-20 10:52:06 +0100

    feat: add analyze script for testing

--- src/test/apps/ssr-code-split/next.config.js
+++ src/test/apps/ssr-code-split/next.config.js
@@ -1,3 +1,7 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
-}
+});
