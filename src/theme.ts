55165fb251df54c951a8ca905d58219def929573
garronej <joseph.garrone@data.gouv.fr>
2023-07-14 23:16:50 +0200

    New color palette generated by chatGPT

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,32 @@
-import { createThemeProvider, defaultGetTypographyDesc } from "onyxia-ui";
+import { createThemeProvider, defaultGetTypographyDesc, defaultPalette } from "onyxia-ui";
+
+export const palette: typeof defaultPalette = {
+	...defaultPalette,
+    "focus": {
+        "main": "#007ACC",
+        "light": "#33A2D9",
+        "light2": "#85C5E5",
+    },
+    "dark": {
+        "main": "#1F2933",
+        "light": "#323F4B",
+        "greyVariant1": "#3E4C59",
+        "greyVariant2": "#52606D",
+        "greyVariant3": "#7B8794",
+        "greyVariant4": "#A1AEB7",
+    },
+    "light": {
+        "main": "#F8F9FA",
+        "light": "#FFFFFF",
+        "greyVariant1": "#E4E7EB",
+        "greyVariant2": "#CBD2D9",
+        "greyVariant3": "#9AA5B1",
+        "greyVariant4": "#6E7A88",
+    }
+};
 
 export const { ThemeProvider, useTheme, useStyles } = createThemeProvider({
+	palette,
 	"getTypographyDesc": params => ({
 		...defaultGetTypographyDesc(params),
 		"fontFamily": '"Work Sans", sans-serif'
