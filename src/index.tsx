a78c497735227d03883c876ff39be2911ff83553
garronej <joseph.garrone@data.gouv.fr>
2023-07-17 03:59:47 +0200

    Done with bullet points

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,6 @@ import { breakpointsValues } from "onyxia-ui";
 import logoPngUrl from "./assets/logo.png";
 import tssDemoMp4Url from "./assets/tss-demo-hevc-safari.mp4";
 import tssDemoWebmUrl from "./assets/tss-demo-vp9-chrome.webm";
-import trendingPngUrl from "./assets/tss-trending.png";
 
 const repoUrl = "https://github.com/garronej/tss-react";
 
@@ -158,20 +157,47 @@ function App() {
                 "description": `TSS [is used by thousands of dev teams](https://npmtrends.com/@remix-run/react-vs-solid-js-vs-tss-react). Issues are resolved swiftly, and the library will remain actively maintained for the foreseeable future.`
               },
               {
-                "title": "Dynamic Style Generation (for better and for worse)",
-                "IconOverride": createEmojiIcon("💫"),
-                "description": `TSS enables to generate styles based on the props and internal states of components.  
-                This unfortunately prevents us from supporting [Server Component (RSC)](https://nextjs.org/docs/getting-started/react-essentials#server-components) in Next.js.  
-                We remain hopeful for future support of RSC, contingent on [the provision of a suitable solution by Vercel and React](https://github.com/vercel/next.js/blob/dc6c22c99117bb48beedc4eed402a57b21f03963/docs/02-app/01-building-your-application/04-styling/03-css-in-js.mdx#L10-L12).  
-                If you need RSC support today, you can consider *zero runtime* solutions like Panda-CSS or Vanilla Extract, 
-                but the expression of complex styles is significantly harder in this paradigm.
-                `
+                "title": "Eliminate CSS priority conflicts",
+                "IconOverride": createEmojiIcon("😌"),
+                "description": `With TSS you can determine the precedence of multiple classes applied to a component and [arbitrarily increase specificity of some rules](https://docs.tss-react.dev/increase-specificity)`
+              },
+              {
+                "title": "No new syntax",
+                "IconOverride": createEmojiIcon("🙅‍♂️"),
+                "description": `TSS does not introduce new syntax. It's just plain CSS, albeit expressed under the form of a JavaScript object. 
+                Pseudo-class selectors like \`:hover\` and media queries are expressed as you would expect, [as properties of the style object](https://docs.tss-react.dev/).`
               },
               {
                 "title": "Your JSX remains readable",
                 "IconOverride": createEmojiIcon("🤩"),
                 "description": `Unlike other styling solution that tend to clutter the JSX, TSS enables [isolating the styles from the component structure](https://stackblitz.com/edit/vercel-next-js-bmc6dm?file=ui/TssLogo.tsx).  
                 That been said, sometime it's just easier to inline the styles directly within your components, [TSS enables this as well](https://stackblitz.com/edit/vercel-next-js-bmc6dm?file=ui/TssLogo_intertwined.tsx).`
+              },
+              {
+                "title": "Powered by [Emotion](https://emotion.sh/)",
+                "IconOverride": createEmojiIcon("🚀"),
+                "description": `Built on top of [@emotion/react](https://emotion.sh/docs/@emotion/react) TSS integrates seamlessly with the ecosystem of UI libraries that use emotion under the hood.
+                It's the case of [MUI](https://mui.com/), [Theme UI](https://theme-ui.com/), [Chakra-UI](https://chakra-ui.com/) and many.  
+                If you already have [@emotion/react](https://emotion.sh/docs/@emotion/react) in your project adding TSS has virtually no impact on your bundle size (~5kB minziped) and your project configuration.  
+                `
+              },
+              {
+                "title": "Dynamic Style Generation (for better and for worse)",
+                "IconOverride": createEmojiIcon("💫"),
+                "description": `TSS enables to generate styles based on the props and internal states of components.  
+                This unfortunately prevents us from supporting [Server Component (RSC)](https://nextjs.org/docs/getting-started/react-essentials#server-components) in Next.js.  
+                We remain hopeful for future support of RSC, contingent on [the provision of a suitable solution by Vercel and React](https://github.com/vercel/next.js/blob/dc6c22c99117bb48beedc4eed402a57b21f03963/docs/02-app/01-building-your-application/04-styling/03-css-in-js.mdx#L10-L12).  
+                If you need RSC support today, you can consider *zero runtime* solutions like [Panda-CSS](https://panda-css.com/) or [Vanilla Extract](https://vanilla-extract.style/), 
+                but the expression of complex styles is significantly harder in this paradigm.
+                `
+              },
+              {
+                "title": "Component library author friendly",
+                "IconOverride": createEmojiIcon("📦"),
+                "description": `If you are the author of a component library, that extends MUI or an other toolkit that leverages emotion
+                you can use TSS internally without making it a peer dependency of your module.
+                Example: [mui-datatables](https://github.com/gregnb/mui-datatables) is using TSS internally.  
+                Read more about this [here](https://docs.tss-react.dev/publish-a-module-that-uses-tss).`
               },
 
             ]}
@@ -181,7 +207,7 @@ function App() {
 
       footer={
         <GlFooter
-          bottomDivContent="[GitHub](https://github.com/garronej/tss-react) - [Documentation](https://github.com/garronej/tss-react)"
+          bottomDivContent="[GitHub](https://github.com/garronej/tss-react) - [Documentation](https://github.com/garronej/tss-react) - [Edit this website](https://github.com/garronej/tss-react/blob/landingpage/src/index.tsx)"
           links={[
             {
               "href": "https://www.npmjs.com/package/tss-react",
