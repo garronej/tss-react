393b16725f007a12e10618a293cb66871a952d65
garronej <joseph.garrone@data.gouv.fr>
2023-07-17 04:53:35 +0200

    Update index

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { GlLogo } from "gitlanding/utils/GlLogo";
 import { GlSectionDivider } from "gitlanding/GlSectionDivider";
 import { ThemeProvider, useStyles } from "./theme";
 import { GlArticle } from "gitlanding/GlArticle";
+import { GlButton } from "gitlanding/utils/GlButton";
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { TssPlayfulLogo } from "./TssPlayfulLogo";
@@ -97,24 +98,15 @@ function App() {
             hasLinkToSectionBellow={true}
           />
           <GlArticle
-            id="firstSection"
-            title="🔩 State of the art TypeScript support"
-            body={`Save yourself back and fourth between your code and the 
-        documentation and refactor with confidence.  
-        **Even used in vanilla JavaScript** project, you will benefit from the type inference.
-        `}
-            buttonLabel="Get started 🚀"
-            buttonLink={{
-              "href": "https://docs.tss-react.dev",
-            }}
             classes={{
-              "video": css({
-                "borderRadius": 10
-              })
+              "aside": css({
+                "maxWidth": "min(calc(100vw - 2 * 10px), 1030px)",
+              }),
             }}
             illustration={{
               "type": "video",
               "hasShadow": false,
+              "controls": true,
               "sources":
                 [
                   {
@@ -128,27 +120,41 @@ function App() {
                 ]
             }}
             hasAnimation={true}
-            illustrationPosition="left"
           />
+          <GlButton 
+            href="https://stackblitz.com/edit/vercel-next-js-bmc6dm?file=ui%2FTssLogo.tsx"
+            doOpenNewTabIfHref={true}
+            className={
+              css({
+                "display": "inline-block",
+                "margin": "0 auto",
+
+              })
+            }
+          >
+            Try this in StackBlitz 🚀
+          </GlButton>
 
           <GlSectionDivider />
 
           <GlCheckList
+            className={css({ "marginTop": 0 })}
+            heading="Why TSS?"
+            subHeading="Why this styling solution instead of the [2261 others](https://www.npmjs.com/search?q=css-in-js)?"
             hasAnimation={true}
-            Icon={() => null}
             elements={[
               {
                 "title": "Works great with [`MUI`](https://mui.com)",
                 "IconOverride": createEmojiIcon("🤝"),
-                "description": `MUI provides [a step-by-step guide, along with a codemod tool](https://mui.com/guides/migration-v4/#2-use-tss-react), for transitioning from MUI version 4 to version 5 using TSS.  
+                "description": `MUI features [a step-by-step guide, along with a codemod tool](https://mui.com/guides/migration-v4/#2-use-tss-react), for transitioning from MUI version 4 to version 5 using TSS.  
                 You don't have to use MUI to use TSS, TSS is a standalone solution, but the two libraries are a great match.`,  
               },
               {
                 "title": "Next.js App Router support",
                 "IconOverride": createEmojiIcon("🔧"),
-                "description": `Setting 
-                  up TSS (and MUI) to work with Next.js's SSR is a breeze. You just need to wrap your app withing our [\`<NextAppDirEmotionCacheProvider/>\`](https://docs.tss-react.dev/ssr/next.js).  
-                  MUI is using [our provider](https://github.com/mui/material-ui/blob/4036eb60b45c260e9f49904f1b54f8c947a944d2/examples/material-next-app-router-ts/src/components/ThemeRegistry/EmotionCache.tsx#L20-L21).  
+                "description": `Setting up TSS (and MUI) to work with Next.js's SSR is a breeze. 
+                You just need to wrap your app withing our [\`<NextAppDirEmotionCacheProvider/>\`](https://docs.tss-react.dev/ssr/next.js).  
+                MUI is using [our provider](https://github.com/mui/material-ui/blob/4036eb60b45c260e9f49904f1b54f8c947a944d2/examples/material-next-app-router-ts/src/components/ThemeRegistry/EmotionCache.tsx#L20-L21).  
                 `
               },
               {
@@ -171,7 +177,7 @@ function App() {
                 "title": "Your JSX remains readable",
                 "IconOverride": createEmojiIcon("🤩"),
                 "description": `Unlike other styling solution that tend to clutter the JSX, TSS enables [isolating the styles from the component structure](https://stackblitz.com/edit/vercel-next-js-bmc6dm?file=ui/TssLogo.tsx).  
-                That been said, sometime it's just easier to inline the styles directly within your components, [TSS enables this as well](https://stackblitz.com/edit/vercel-next-js-bmc6dm?file=ui/TssLogo_intertwined.tsx).`
+                On the other hand, sometime it's just easier to inline the styles directly within your components, [TSS enables this as well](https://stackblitz.com/edit/vercel-next-js-bmc6dm?file=ui/TssLogo_intertwined.tsx).`
               },
               {
                 "title": "Powered by [Emotion](https://emotion.sh/)",
@@ -198,13 +204,12 @@ function App() {
                 you can use TSS internally without making it a peer dependency of your module.
                 Example: [mui-datatables](https://github.com/gregnb/mui-datatables) is using TSS internally.  
                 Read more about this [here](https://docs.tss-react.dev/publish-a-module-that-uses-tss).`
-              },
+              }
 
             ]}
           />
         </>
       }
-
       footer={
         <GlFooter
           bottomDivContent="[GitHub](https://github.com/garronej/tss-react) - [Documentation](https://github.com/garronej/tss-react) - [Edit this website](https://github.com/garronej/tss-react/blob/landingpage/src/index.tsx)"
