d523ffb0a647b40a7b7dea52640d40dc75e5f279
garronej <joseph.garrone@protonmail.com>
2025-12-05 16:52:23 +0100

    #233

--- src/mui/index.ts
+++ src/mui/index.ts
@@ -1,3 +1,5 @@
 export { tss, makeStyles, useStyles, withStyles } from "./mui";
 export { useMuiThemeStyleOverridesPlugin } from "./themeStyleOverridesPlugin";
 export type { MuiThemeStyleOverridesPluginParams } from "./themeStyleOverridesPlugin";
+import type * as __internal from "../index";
+export type { __internal };
